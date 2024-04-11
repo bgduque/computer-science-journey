ab03437755f943a4b15ab8839674e4001afd78a1
Boris <152839576+bgduque@users.noreply.github.com>
2024-04-11 19:53:14 +0800

    Update EmployeeDA.java

--- Lab Assignment 5 HashMap Implementation/logic/EmployeeDA.java
+++ Lab Assignment 5 HashMap Implementation/logic/EmployeeDA.java
@@ -1,3 +1,19 @@
+/**
+* Copyright © — New Era University and/or its affiliates. All rights reserved.
+*
+* @author Boris Gamaliel D. Duque
+* @Note : There's no easy step, one must always venture a desolate path before arriving
+*         on their rightful destination. Thus, making each and every one of us attain the
+*         the goal that we have always longed for.
+*
+* @INFORMATION : EmployeeDA — Employee Data Access. Here is where the information about
+*                our employees are stored. It is then read, inputted on our HashMap.
+*                
+*                @param <K>             Key - Our key is an Object String.
+*                @param <V>             Value — Employee Object as it is where our value
+*                                               we stored our value data.
+*/
+
 package logic;
 
 import java.io.BufferedReader;
@@ -5,16 +21,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.Map.Entry;
 
-@SuppressWarnings("unused")
 public class EmployeeDA {
     private final static String[] employeeInfoFilePath = { "./data/emp.csv" };
+    private HashMap<String, Employee> employeeMap = new HashMap<>();
     
-    public HashMap<String, Employee> employeeReader(){
-        HashMap<String, Employee> employeeMap = new HashMap<>();
+    public HashMap<String, Employee> employeeReader() {
         try (BufferedReader r = new BufferedReader(new FileReader(employeeInfoFilePath[0]))) {
             r.readLine();
             String x;
@@ -28,20 +40,11 @@ public class EmployeeDA {
             }
         } catch (FileNotFoundException e){
             for(int i = 0; i < employeeInfoFilePath.length; i++){
-                System.out.printf("File(s) %s not found!", employeeInfoFilePath[i], " , ");
+                System.out.printf("File(s): %-10s not found!\n", employeeInfoFilePath[i].trim());
             }
         } catch (IOException e){
             e.printStackTrace();
         }
         return employeeMap;
-    }
-
-    public String toString() {
-        DepartmentDA dDA = new DepartmentDA();
-        Map<String, Department> departmentMap = dDA.readDepEmp();
-        Department d = new Department();
-        StringBuilder sb = new StringBuilder();
-        sb.append(d.getDepCode());
-        return sb.toString();
     }
 }
