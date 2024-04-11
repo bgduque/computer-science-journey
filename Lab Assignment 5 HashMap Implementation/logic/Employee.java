b1fd8a047cfcc000c0e76daa0e0af17a6d08efa1
Boris <152839576+bgduque@users.noreply.github.com>
2024-04-11 19:52:52 +0800

    Update Employee.java

--- Lab Assignment 5 HashMap Implementation/logic/Employee.java
+++ Lab Assignment 5 HashMap Implementation/logic/Employee.java
@@ -1,23 +1,18 @@
+/**
+* Copyright © — New Era University and/or its affiliates. All rights reserved.
+*
+* @author Boris Gamaliel D. Duque
+* @Note : There's no easy step, one must always venture a desolate path before arriving
+*         on their rightful destination. Thus, making each and every one of us attain the
+*         the goal that we have always longed for.
+*
+*/
+
 package logic;
 
 public class Employee {
     private String empNo, lastName, firstName;
-    private Double salary;
-
-    /*
-     * 
-     * 
-     * 
-     * The getters and setters function for this program has to be overloaded.
-     * This is due to that if the programmer uses this class as an API, 
-     * it is dependable and much be able to handle different types of data.
-     * 
-     * One is being wrapper class and the one is a primitive data type.
-     * 
-     * 
-     * 
-     * Copyright © Boris Gamaliel D. Duque — https://github.com/bgduque
-     */
+    private double salary;
 
     public String getempNo() {
         return empNo;
@@ -30,32 +25,49 @@ public class Employee {
     public String getfirstName() {
         return firstName;
     }
-
-    public Double getSalary() {
-        return salary;
-    }
     
-    public double getSalary(double salary) {
+    public double getSalary() {
         return salary;
     }
 
+    /**
+     * @param empNo          The Employee's Number e.g. E321
+     */
     public void setempNo(String empNo) {
         this.empNo = empNo;
     }
 
+    /**
+     * @param lastName       The Employee's Last Name.
+     */
     public void setlastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * @param firstName      The Employee's First Name.
+     */
     public void setfirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void setsalary(Double salary) {
+    /**
+     * @param salary         The Employee's corresponding salary
+     * @implNote             This is an overloaded function, this one 
+     *                       passes in a data type.
+     */
+    public void setSalary(double salary) {
         this.salary = salary;
     }
 
-    public void setSalary(double salary) {
+    /**
+     * @param salary         The Employee's corresponding salary
+     * @implNote             This is an overloaded function, this one 
+     *                       passes in the Double Object Wrapper.
+     *                       Which is wraps the value of primitive data type.
+     * @see java.lang.Double
+     */
+    public void setSalary(Double salary) { 
         this.salary = salary;
     }
 }
