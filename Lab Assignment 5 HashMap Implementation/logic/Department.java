ba23ccc47da59876a6a3d4a00b21b442f1b74d69
Boris <152839576+bgduque@users.noreply.github.com>
2024-04-11 19:52:37 +0800

    Update Department.java

--- Lab Assignment 5 HashMap Implementation/logic/Department.java
+++ Lab Assignment 5 HashMap Implementation/logic/Department.java
@@ -1,16 +1,16 @@
+/**
+* Copyright © — New Era University and/or its affiliates. All rights reserved.
+*
+* @author Boris Gamaliel D. Duque
+* @Note : There's no easy step, one must always venture a desolate path before arriving
+*         on their rightful destination. Thus, making each and every one of us attain the
+*         the goal that we have always longed for.
+*/
 package logic;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Department {
     private String depCode, depName;
     private double depTotalSalary;
-    private List<Employee> employee;
-
-    public Department() { 
-        employee = new ArrayList<>();
-    }
     
     public String getDepCode() {
         return depCode;
@@ -23,23 +23,35 @@ public class Department {
     public double getDepTotalSalary() {
         return depTotalSalary;
     }
-    
-    public List<Employee> getEmployees() {
-        return employee;
-    }
 
+    /**
+     * @param depCode                The department code value(s).
+     */
     public void setDepCode(String depCode) {
         this.depCode = depCode;
     }
 
+    /**
+     * @param depName                The department name value(s).
+     */
     public void setDepName(String depName) {
         this.depName = depName;
     }
 
+    /**
+     * @param depTotalSalaryEach     Department(s) total calculated salary when passed in the added salary of their corresponding employees.
+     * @implNote                     Overloaded {@function setDepTotalSalary} This one passes a primitive data type. Instead of a wrapper object.
+     */
     public void setDepTotalSalary(double depTotalSalary) {
         this.depTotalSalary = depTotalSalary;
     }
 
+    /**
+     * @param depTotalSalary         Department(s) total calculated salary when passed in the added salary of their corresponding employees.
+     * @implNote                     Overloaded {@function setDepTotalSalary} This one passes a Double Object Wrapper.
+     *                               What does this do is wraps the primitive data type.
+     * @see java.lang.Double
+     */
     public void setDepTotalSalary(Double depTotalSalary) { 
         this.depTotalSalary = depTotalSalary;
     }
